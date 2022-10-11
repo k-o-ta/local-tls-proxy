dc141f82dbbd9f988604e8c9494201712a309112
Koji Ota <afterjnih@gmail.com>
2022-10-11 20:37:39 +0900

    add args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+struct Args {
+    #[clap(long = "cert")]
+    cert: String,
+
+    #[clap(long = "cert-key")]
+    cert_key: String,
+
+    #[clap(long = "src-ip", default_value = "localhost")]
+    src_ip: String,
+
+    #[clap(long = "dest-ip")]
+    dest_ip: String,
+
+    #[clap(long = "src-port", default_value_t = 443)]
+    src_port: i32,
+
+    #[clap(long = "dest-port")]
+    dest_port: i32,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
 }
